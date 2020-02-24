42c563ee3e3d4954a2cd63ed8fc512602075e114
John <jonnydubber@gmail.com>
2020-02-24 17:59:38 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MortgageComponent } from './mortgage/mortgage.component';
+import { SavingsComponent } from './savings/savings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MortgageComponent,
+    SavingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
